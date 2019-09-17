5d3e32154b5a4e11e196fe662c441fb8e9709d1d
ladybug2fire <newbee.hunter@qq.com>
2019-09-17 21:04:37 +0800

    feat: 自动copy cname

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     outputDir: './docs',
     assetsDir: './asset',
-    indexPath: 'index.html'
+    indexPath: 'index.html',
+    configureWebpack: config => {
+        config.plugins.push(
+            new CopyWebpackPlugin([{ from: './CNAME', to: './' }])
+        )
+    }
 }
