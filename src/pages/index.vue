afd22a5859dc86130350888b30d8dd71ae7e6371
ladybug2fire <sunhuacheng@meituan.com>
2020-06-12 10:14:56 +0800

    feat: 添加字体

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,6 +9,8 @@
     <h3>距离{{target|dateformat}}</h3>
     <h1>{{diff>0?'已过去':'还有'}}<span class="diff-date">{{Math.abs(diff)}}</span>天</h1>
     <div class="diff-date">{{now.format('HH:mm:ss')}}</div>
+    <div class="words">为人民服务</div>
+
   </div>
 </template>
 
@@ -35,7 +37,6 @@ export default {
     }
   },
   mounted() {
-    console.log(store.get("target"));
     setInterval(() => (this.now = moment()), 1000);
   },
   methods: {
@@ -47,8 +48,21 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@font-face {
+  font-family: 'Exo';
+  src: url('../assets/fonts/Exo_2/static/Exo2-Thin.ttf');
+}
+@font-face {
+  font-family: 'LiuJianMaoCao';
+  src: url('../assets/fonts/Liu_Jian_Mao_Cao/LiuJianMaoCao-Regular.ttf');
+}
 .diff-date {
   font-size: 10rem;
   padding: 10px;
+  font-family: Exo, Arial, Helvetica, sans-serif;
+}
+.words{
+  font-family: LiuJianMaoCao, Arial, Helvetica, sans-serif;
+  font-size: 5rem;
 }
 </style>
